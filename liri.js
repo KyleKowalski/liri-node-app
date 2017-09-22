6aa2a614e375281049672aec41b957221eca35ce
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-09-22 11:33:41 -0600

    more initial setup for the homework

---
+++ liri.js
@@ -0,0 +1,23 @@
+// grab data from keys and store it into a variable
+
+// TODO create these:
+//    * `my-tweets`
+// * This will show your last 20 tweets and when they were created at in your terminal/bash window.
+
+//    * `spotify-this-song`
+// * This will show the following information about the song in your terminal/bash window
+
+// * Artist(s)
+
+// * The song's name
+
+// * A preview link of the song from Spotify
+
+// * The album that the song is from
+
+// * If no song is provided then your program will default to "The Sign" by Ace of Base.
+//    * `movie-this`
+
+//    * `do-what-it-says`
+
+// TODO - log out each command as it is input into log.txt
