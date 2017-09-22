1dfc92b36fdbf9c91799ca9ff9965d10243d7c10
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-09-22 14:34:55 -0600

    added twitter keys

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'XpwYmcM6kmT4wDRaK7bHzkzkf',
+  consumer_secret: 'fbNgKAwUke33mWbC9AEk9NZ823WMpXkVqNqVVHeWcOXGFV35ja',
+  access_token_key: '80392403-Rhy2uiDhmuq2Rz5NQ4E9wEM5KV9jGSAFTh0AOFrCx',
+  access_token_secret: 'VEZKh2UhfH4mSdYnB9QNAlwPEQwYUcnCfFEq56uSw8GfF',
 }
 
 module.exports = twitterKeys;
