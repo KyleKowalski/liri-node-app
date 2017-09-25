23fca8566f682714a6f51a3845840ea4865dcd02
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-09-25 16:27:43 -0600

    OMDB is a WIP - but life calls

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ var twitter = require('twitter');
 var twitterClient = new twitter(keysFile.twitterKeys);
 var Spotify = require('node-spotify-api');
 var spotifyClient = new Spotify(keysFile.spotifyKeys);
+var request = require("request") // for OMDB
 
    
 var params = {screen_name: 'KyleKowalski'};
@@ -16,10 +17,10 @@ function mainPrompt() {
         {
             type: "list",
             message: "\n\n\n=====\nWelcome to Liri-Bot - please make a selection:\n=====",
-            choices: ["Twitter", "Spotify", "OMDB", "Quit"],
+            choices: ["Twitter", "Spotify", "OMDB", "Do What It Says", "Quit"],
             name: "mainPromptChoice"
         }
-    ]).then(function(response) {
+    ]).then(function(response) { // TODO replace with case
         if (response.mainPromptChoice === 'Twitter') {
             getTwitter();
         }
@@ -27,7 +28,10 @@ function mainPrompt() {
             getSpotify();
         }
         else if (response.mainPromptChoice === 'OMDB') {
-            console.log("I guess we need OMDB now..."); // TODO
+            getOMDB();
+        }
+        else if (response.mainPromptChoice === 'Do What It Says') {
+            doWhatTheTextFileSays();
         }
         else if (response.mainPromptChoice === 'Quit') {
             quit();
@@ -143,8 +147,64 @@ function quit() {
     console.log("\n=====\nHave a great day!\n\nGood Bye!\n=====");
 }
 
-//    * `movie-this`
+function getOMDB() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What movie would you like to look up?",
+            name: "movieRequest"
+        }
+    ]).then(function(response){
+        console.log("Spotify Request With : >" + response.movieRequest + "<")
 
-//    * `do-what-it-says`
+        if (response.movieRequest === ""){
+            response.movieRequest = "Mr Nobody" // TODO verify this gives ace of base
+            console.log('Since you gave no search term, you get "Mr Nobody" - enjoy!');
+        }
+
+        ;
+        
+        // Then run a request to the OMDB API with the movie specified
+        request("http://www.omdbapi.com/?t=" + response.movieRequest + "s&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+        
+          // If the request is successful (i.e. if the response status code is 200)
+          if (!error && response.statusCode === 200) {
+        
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+          }
+        });
+        
+
+
+        // spotifyClient.search({ type: 'track', query: response.movieRequest }, function(err, data) {
+        //     if (err) {
+        //         return console.log('Error occurred: ' + err);
+        //     }
+        //     // console.log(data)
+        //     var songCount = 1;
+        //     data.tracks.items.forEach(function(songListing){
+        //         let artist = songListing.album.artists[0].name;
+        //         let song = songListing.name;
+        //         let songUrl = songListing.album.artists[0].external_urls.spotify;
+        //         let album = songListing.album.name;
+        //         console.log("\n=====")
+        //         console.log(songCount + ". Artist: " + artist);
+        //         console.log(songCount + ". Song: " + song);
+        //         console.log(songCount + ". Song URL: " + songUrl);
+        //         console.log(songCount + ". Album: " + album);
+        //         songCount++;
+        //         console.log("=====")
+        //     });
+        //     mainPrompt();
+        // });
+    });
+}
+
+function doWhatTheTextFileSays() {
+    // I'm going to read the text file and do something... because... 
+    // TODO get the text file and do something
+}
 
 // TODO - log out each command as it is input into log.txt
